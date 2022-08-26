4ec96df9106b295e784baba11e0588e42b155550
matheuzcapdevila <matheuz.lcc@gmail.com>
2022-08-26 16:29:25 -0300

    teste

---
+++ mapa/TilesetElement.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="TilesetElement" tilewidth="64" tileheight="64" tilecount="12" columns="4">
+ <image source="../../NinjaAdventure/NinjaAdventure/Backgrounds/Tilesets/TilesetElement.png" trans="ff00ff" width="256" height="240"/>
+</tileset>
