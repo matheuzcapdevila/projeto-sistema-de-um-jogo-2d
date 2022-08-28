50e5b360f40aeb306d4bfa3c1bde2330a6710c16
matheuzcapdevila <matheuz.lcc@gmail.com>
2022-08-28 19:57:37 -0300

    armas2

--- weapon.py
+++ weapon.py
@@ -4,7 +4,8 @@ class Weapon(pygame.sprite.Sprite):
     def __init__(self, player, groups):
         super().__init__(groups)
         direction
-        #graphic
+        # graphic
         self.image = pygame.Surface((40,40))
-        
+
+        # placement
         self.rect = self.image.get_rect(center = player.rect.center)
